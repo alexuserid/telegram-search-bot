896856d269659b0cea5d94ae2bfeca8aea032f2d
Alex <34424602+alexuserid@users.noreply.github.com>
2018-01-29 10:47:12 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,24 +7,20 @@ import constants
 try:
     bot=telebot.TeleBot(constants.tokenTelegram)
 
-
     @bot.message_handler(content_types=["text"])
     def repeat_all_messages(message):
         try:
             searchRequest = urlopen(Request('https://www.startpage.com/do/search?q=' + urllib.parse.quote(message.text) + '&l=russian',
-                                            headers={'User-Agent': 'Mozilla'})).read()
-            bsObj = BeautifulSoup(searchRequest, "html.parser")
+                headers={'User-Agent': 'Mozilla'})).read()
             resultList = bsObj.findAll('div', {'class': 'result'})
             ansver = "____"
             for result in resultList:
                 ansver += result.get_text()
-            bot.send_message(message.chat.id, ansver)
-        except Exception as e:
-         bot.send_message(message.chat.id, e)
+                bot.send_message(message.chat.id, ansver)
+            except Exception as e:
+                bot.send_message(message.chat.id, e)
 
+                if __name__ == '__main__':
+                    bot.polling(none_stop=True)
 
-
-    if __name__ == '__main__':
-        bot.polling(none_stop=True)
-
-except E as e:
+                except E as e:
